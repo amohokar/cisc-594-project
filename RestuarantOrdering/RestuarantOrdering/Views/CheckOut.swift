c8aeb60c988756b90a24294e9d1da1ebc61c099c
khushiv0811 <65448570+khushiv0811@users.noreply.github.com>
2023-10-06 12:15:42 -0400

    add missing checkoutview

--- RestuarantOrdering/RestuarantOrdering/Views/CheckOut.swift
+++ RestuarantOrdering/RestuarantOrdering/Views/CheckOut.swift
@@ -1,60 +1,97 @@
 //
-//  CheckOut.swift
-//  Food
+//  WeclomeView.swift
+//  RestuarantOrdering
 //
-//  Created by BqNqNNN on 7/14/20.
+//  Created by Kushboo Patel on 10/4/23.
 //
+
 
 import SwiftUI
 
 struct CheckOut: View {
+    @State var hero = false
+    @State var addtoCart = false
+    @State var data = cart
     var body: some View {
-        ScrollView{
-            
-            ZStack {
-                Color(#colorLiteral(red: 0.9021687508, green: 0.1747280955, blue: 0.318703413, alpha: 1))
-                    .frame(width: 600, height: 600)
-                    .edgesIgnoringSafeArea(.all)
-                    .cornerRadius(300)
-                    .offset(x: -50, y: -350)
-                Color(#colorLiteral(red: 0.9617715478, green: 0.1775636971, blue: 0.3371206522, alpha: 1))
-                    .frame(width: 300, height: 300)
-                    .edgesIgnoringSafeArea(.all)
-                    .cornerRadius(300)
-                    .offset(x: -100, y: -250)
-
-                HStack {
-                    Text("Order")
-                        .bold()
-                        .font(.title)
-                    Spacer()
-                    Text("Close")
-                        .font(.title2)
-                }
-                
-                .padding(.all, 20)
-                .foregroundColor(.white)
-                .frame(width: UIScreen.main.bounds.width)
-                .padding(.top, -250)
-                
-            }
-            HStack {
-                Text("Order")
-                    .bold()
-                    .font(.title)
-                Spacer()
-                Text("Close")
-                    .font(.title2)
-            }
-            Text("Work in progress")
+        HStack {
+            Text("CheckOut Items")
                 .bold()
-                .font(.title)
-
+                .multilineTextAlignment(.trailing)
+                .padding(.leading, 20)
+            
             Spacer()
         }
-        
-        .edgesIgnoringSafeArea(.all)
-    }
+            Spacer()
+            NavigationLink(
+                destination: TipsView().navigationBarBackButtonHidden(false).navigationBarHidden(false),
+                label: {
+                    HStack {
+                        Image(systemName: "cart.fill")
+                            .foregroundColor(Color(#colorLiteral(red: 0.9580881, green: 0.10593573, blue: 0.3403331637, alpha: 1)))
+                        
+                        Text("Tips")
+                            .bold()
+                            .foregroundColor(Color(#colorLiteral(red: 0.9580881, green: 0.10593573, blue: 0.3403331637, alpha: 1)))
+                        
+                    }
+                    .frame(width: 300, height: 60, alignment: .center)
+                    .border(Color(#colorLiteral(red: 0.9580881, green: 0.10593573, blue: 0.3403331637, alpha: 1)), width: 3)
+                    .cornerRadius(5)
+                })
+        ScrollView {
+                // Card View
+                VStack(spacing: 30) {
+                    ForEach(0..<self.data.count){ i in
+                        let card = data[i]
+                        VStack {
+                            ZStack {
+                                Image(card.image)
+                                    .resizable()
+                                    .frame(width:  (UIScreen.main.bounds.width)*0.9 , height:  (UIScreen.main.bounds.height)*0.25 )
+                                    .cornerRadius(20)
+                                    .shadow(radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
+                            }.edgesIgnoringSafeArea(.top)
+                            
+                            HStack {
+                                Text(card.title)
+                                    .bold()
+                                    .padding(.all, /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
+                                Spacer()
+                            }
+                            .padding(.leading, 20)
+                            
+                            HStack {
+                                Text(card.descrip)
+                                    .font(.subheadline)
+                                    .foregroundColor(.gray)
+                                    .padding(.leading, 30)
+                                Spacer()
+                            }
+                            HStack {
+                                ForEach(0 ..< card.stars) { _ in
+                                    Image(systemName: "star.fill")
+                                        .foregroundColor(.yellow)
+                                        .font(.subheadline)
+                                }
+                                Spacer()
+                                
+                                Text(card.price)
+                                    .font(.subheadline)
+                                    .bold()
+                            }
+                            .padding(.bottom, 30)
+                            .padding(.leading, /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
+                            .padding(.trailing, /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
+                            .padding(.leading, 20)
+                            .padding(.trailing, 20)
+                        }
+                            
+                    }
+                    
+                    
+                }            }
+            Spacer()
+        }
 }
 
 struct CheckOut_Previews: PreviewProvider {
@@ -62,3 +99,4 @@ struct CheckOut_Previews: PreviewProvider {
         CheckOut()
     }
 }
+
